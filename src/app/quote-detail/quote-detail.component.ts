701d0af4c302da362986a99d043813c6f518dcf1
Ketsia-a <iraketsia2001@gmail.com>
2020-11-09 14:50:49 +0200

    created a detail component to help display

---
+++ src/app/quote-detail/quote-detail.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input,Output, OnInit, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote-detail',
+  templateUrl: './quote-detail.component.html',
+  styleUrls: ['./quote-detail.component.css']
+})
+export class QuoteDetailComponent implements OnInit {
+ @Input() quote: Quote;
+ @Output() isComplete = new EventEmitter<boolean>();
+ quoteDelete(complete: boolean) {
+   this.isComplete.emit(complete);
+  }
+  upvote() {
+    this.quote.upvote = this.quote.upvote + 1;
+  }
+  downvote() {
+    this.quote.downvote = this.quote.downvote + 1;
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
